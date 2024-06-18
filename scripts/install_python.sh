fb678eb11c2def6676cd88959da2fd95ea892249
Sankhag <sankhag@mitesp.com>
2024-06-18 15:25:03 +0530

    Edit codes

--- scripts/install_python.sh
+++ scripts/install_python.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 sudo yum update -y && sudo yum install -y python3
+
+sudo yum install -y wget unzip
+wget terraform.zip https://releases.hashicorp.com/terraform/1.7.4/terraform_1.7.4_linux_amd64.zip
+unzip terraform.zip
+sudo mv terraform /usr/local/bin/
+rm -rf terraform.zip
+
