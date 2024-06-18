36d9933c7c3b41f2f3e2d32ebb2cc4c84e022f7d
Sankhag <sankhag@mitesp.com>
2024-06-18 20:50:48 +0530

    Edit codes

--- scripts/install_python.sh
+++ scripts/install_python.sh
@@ -7,10 +7,10 @@ wget https://releases.hashicorp.com/terraform/1.7.4/terraform_1.7.4_linux_amd64.
 unzip terraform_1.7.4_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
 rm -rf terraform.zip
-sudo apt install nodejs
 sudo yum install nodejs
 
 curl -fsSL https://rpm.nodesource.com/setup_18.x | sudo bash -
 sudo yum install -y nodejs
 sudo npm install -g typescript@5.1.6
 sudo yum -y install ansible
+sudo yum -y install jq
